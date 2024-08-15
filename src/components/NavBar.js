683828bc18ece7f76f2b269cbabd9b803dfbc6c0
MichaelNgure <mikynjoro01@gmail.com>
2024-08-15 14:52:42 +0300

    router code along

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css"
+
+function NavBar(){
+    return(
+        <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about" className="nav-link">About</NavLink>
+            <NavLink to="/login" className="nav-link">Login</NavLink>
+        </nav>
+    )
+}
+
+export default NavBar
